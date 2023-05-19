6ddcd3d9f4155af104cf2aab46d1fc6d115d9874
Peng Liu <pliu@lattice-engines.com>
2023-05-19 12:21:45 +0800

    格式化代码

--- src/views/wiki/WikiSearchResult.vue
+++ src/views/wiki/WikiSearchResult.vue
@@ -1,194 +1,193 @@
 <template>
-    <div class="wrapper">
-      <div class="main theme-a">
-        <div class="search-wrapper">
-          <div class="search-input-wrapper">
-            <i class="icon-search"></i>
-            <input class="search-input" v-focus tabindex="1" v-model="searchText" type="text" placeholder="输入搜索内容" @keydown.enter="search" required>
-            <button class="search-button" type="submit" @click="search">Search</button>
-          </div>
-          <div v-if="showWarning" class="search-warning">
-            <p>没有搜索到任何内容！！</p>
-          </div>
-          <div class="search-item" v-for="item in wikiSearchResultList.list" :key="item.id">
-            <div class="search-title" v-html="highlightSearchResult(item.search, item.title, true)"></div>
-            <div class="search-content" v-html="highlightSearchResult(item.search, item.markdownContentPureText)"></div>
-          </div>
+  <div class="wrapper">
+    <div class="main theme-a">
+      <div class="search-wrapper">
+        <div class="search-input-wrapper">
+          <i class="icon-search"></i>
+          <input class="search-input" v-focus tabindex="1" v-model="searchText" type="text" placeholder="输入搜索内容"
+            @keydown.enter="search" required>
+          <button class="search-button" type="submit" @click="search">Search</button>
+        </div>
+        <div v-if="showWarning" class="search-warning">
+          <p>没有搜索到任何内容！！</p>
+        </div>
+        <div class="search-item" v-for="item in wikiSearchResultList.list" :key="item.id">
+          <div class="search-title" v-html="highlightSearchResult(item.search, item.title, true)"></div>
+          <div class="search-content" v-html="highlightSearchResult(item.search, item.markdownContentPureText)"></div>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
     
-  <script>
+<script>
 import { reactive, toRefs, ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import { get, deleteAPI, post, put } from '../../utils/request';
 import { marked } from 'marked';
 
-  export default {
-    name: 'WikiSearchResult',
-    setup() {
-      const searchText = ref("");
-      const showWarning = ref(false);
-      const wikiSearchResultList = reactive({
-        list: []
-      });
+export default {
+  name: 'WikiSearchResult',
+  setup() {
+    const searchText = ref("");
+    const showWarning = ref(false);
+    const wikiSearchResultList = reactive({
+      list: []
+    });
 
-      onMounted(() => {
-        // 使用router传来的参数searchText来直接触发搜索
-        const route = useRoute();
-        if (route.params.searchText) {
-          searchText.value = route.params.searchText;
-          search();
-        }
-      });
+    onMounted(() => {
+      // 使用router传来的参数searchText来直接触发搜索
+      const route = useRoute();
+      if (route.params.searchText) {
+        searchText.value = route.params.searchText;
+        search();
+      }
+    });
 
-      const search = async () => {
-        showWarning.value = false;
-        wikiSearchResultList.list = [];
-        if (!searchText.value) {
-          return;
-        }
-        const response = await get('/api/wiki/search', {searchText: searchText.value});
-        console.log(response);
-        if (response.Success) {
-          for (const item of response.Result) {
-            item.target = `/wiki/${item.categoryId}/${item.id}`;
-            item.markdownContent = marked(item.markdownContent);
-            // 去除markdown语法的字符与图片的html字符后的wiki纯文本内容
-            item.markdownContentPureText = getPureTextFromHtmlElement(createElementFromHTML(item.markdownContent));
-            item.search = searchText.value;
+    const search = async () => {
+      showWarning.value = false;
+      wikiSearchResultList.list = [];
+      if (!searchText.value) {
+        return;
+      }
+      const response = await get('/api/wiki/search', { searchText: searchText.value });
+      console.log(response);
+      if (response.Success) {
+        for (const item of response.Result) {
+          item.target = `/wiki/${item.categoryId}/${item.id}`;
+          item.markdownContent = marked(item.markdownContent);
+          // 去除markdown语法的字符与图片的html字符后的wiki纯文本内容
+          item.markdownContentPureText = getPureTextFromHtmlElement(createElementFromHTML(item.markdownContent));
+          item.search = searchText.value;
 
-            // 使用toLowerCase来忽略大小写进行搜索
-            // 经过后端search接口返回的结果是包含markdown语法字符与图片的html字符的，这里再使用includes方法搜索去除markdown字符与图片的html字符后的wiki内容与标题是否包含searchText
-            if (item.title.toLowerCase().includes(searchText.value.toLowerCase()) || item.markdownContentPureText.toLowerCase().includes(searchText.value.toLowerCase())) {
-              wikiSearchResultList.list.push(item)
-            }
+          // 使用toLowerCase来忽略大小写进行搜索
+          // 经过后端search接口返回的结果是包含markdown语法字符与图片的html字符的，这里再使用includes方法搜索去除markdown字符与图片的html字符后的wiki内容与标题是否包含searchText
+          if (item.title.toLowerCase().includes(searchText.value.toLowerCase()) || item.markdownContentPureText.toLowerCase().includes(searchText.value.toLowerCase())) {
+            wikiSearchResultList.list.push(item)
           }
-        } 
-
-        if (wikiSearchResultList.list.length == 0) {
-          showWarning.value = true;
         }
       }
 
-      const createElementFromHTML = (htmlString) => {
-        var div = document.createElement('div');
-        div.innerHTML = htmlString.trim();
-        return div;
-      }
-
-      const getPureTextFromHtmlElement = (htmlElement) => {
-        var text = htmlElement.innerText || htmlElement.textContent;
-        console.log('text', text);
-        return text.replace(/(\r\n|\n|\r)/gm, "");
-      }
-
-      const highlightSearchResult = (searchText, pureText, forTitle = false) => {
-        return pureText.replace(new RegExp(searchText, "gi"), match => {
-          const highlightClass = forTitle ? 'highlightTextSearchTitle' : 'highlightTextSearchContent';
-          return '<span class="' + highlightClass + '">' + match + '</span>';
-        });
-      }
-
-      return {
-        searchText,
-        showWarning,
-        search,
-        wikiSearchResultList,
-        highlightSearchResult
+      if (wikiSearchResultList.list.length == 0) {
+        showWarning.value = true;
       }
     }
+
+    const createElementFromHTML = (htmlString) => {
+      var div = document.createElement('div');
+      div.innerHTML = htmlString.trim();
+      return div;
+    }
+
+    const getPureTextFromHtmlElement = (htmlElement) => {
+      var text = htmlElement.innerText || htmlElement.textContent;
+      console.log('text', text);
+      return text.replace(/(\r\n|\n|\r)/gm, "");
+    }
+
+    const highlightSearchResult = (searchText, pureText, forTitle = false) => {
+      return pureText.replace(new RegExp(searchText, "gi"), match => {
+        const highlightClass = forTitle ? 'highlightTextSearchTitle' : 'highlightTextSearchContent';
+        return '<span class="' + highlightClass + '">' + match + '</span>';
+      });
+    }
+
+    return {
+      searchText,
+      showWarning,
+      search,
+      wikiSearchResultList,
+      highlightSearchResult
+    }
   }
-  </script>
+}
+</script>
     
-  <style lang="scss" scoped>
-  .wrapper {
-    // 背景图片
-    background-color: #2d76c8;
-    background-image: url('../../assets/shadow_light.png'), url('../../assets/pixels.png');
-    background-position: 0 0, 0 0;
-    background-repeat: repeat;
-  }
-  
-  .main {
-    max-width: 17.6rem;
-    width: 100%;
-    margin-left: auto;
-    margin-right: auto;
-    padding-top: .2rem;
-    padding-bottom: .5rem;
-  }
+<style lang="scss" scoped>
+.wrapper {
+  // 背景图片
+  background-color: #2d76c8;
+  background-image: url('../../assets/shadow_light.png'), url('../../assets/pixels.png');
+  background-position: 0 0, 0 0;
+  background-repeat: repeat;
+}
 
-  .search-wrapper {
-    background-color: var(--wiki-content-background-color);
-    padding: .1rem;
-  }
+.main {
+  max-width: 17.6rem;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: .2rem;
+  padding-bottom: .5rem;
+}
 
-  .search-input-wrapper {
-    position: relative;
-    margin-bottom: .1rem;
-  }
+.search-wrapper {
+  background-color: var(--wiki-content-background-color);
+  padding: .1rem;
+}
 
-  .search-input {
-    width: 80%;
-    margin-right: .1rem;
-    padding-left: .3rem !important;
-    height: .18rem;
-    max-width: 100%;
-    padding: 4px 6px;
-    border: 1px solid #ddd;
-    background: #fff;
-    color: #444;
-    border-radius: 4px;
-    font-size: .15rem;
-  }
+.search-input-wrapper {
+  position: relative;
+  margin-bottom: .1rem;
+}
 
-  .search-button {
-    background-color: #00a8e6;
-    color: #fff;
-    font-size: .15rem;
-    text-decoration: none;
-    text-align: center;
-    border: 1px solid rgba(0,0,0,.06);
-    border-radius: 4px;
-    vertical-align: middle;
-    text-decoration: none;
-    text-align: center;
-    padding: .05rem .15rem;
-  }
-  .search-button:hover {
-    background-color: #35b3ee;
-    color: #fff;
-  }
+.search-input {
+  width: 80%;
+  margin-right: .1rem;
+  padding-left: .3rem !important;
+  height: .18rem;
+  max-width: 100%;
+  padding: 4px 6px;
+  border: 1px solid #ddd;
+  background: #fff;
+  color: #444;
+  border-radius: 4px;
+  font-size: .15rem;
+}
 
-  .search-button:active {
-    background-color: #0091ca;
-    color: #fff;
-  }
+.search-button {
+  background-color: #00a8e6;
+  color: #fff;
+  font-size: .15rem;
+  text-decoration: none;
+  text-align: center;
+  border: 1px solid rgba(0, 0, 0, .06);
+  border-radius: 4px;
+  vertical-align: middle;
+  text-decoration: none;
+  text-align: center;
+  padding: .05rem .15rem;
+}
 
-  .search-warning {
-    background: #fffceb;
-    color: #e28327;
-    border-color: rgba(226,131,39,.3);
-    margin-bottom: .15rem;
-    padding: .05rem;
-    border: .01rem solid rgba(45,112,145,.3);
-    border-radius: .04rem;
-    text-shadow: 0 .01rem 0 #fff;
-  }
+.search-button:hover {
+  background-color: #35b3ee;
+  color: #fff;
+}
 
-  .search-item {
-    margin-bottom: .4rem;
-  }
+.search-button:active {
+  background-color: #0091ca;
+  color: #fff;
+}
 
-  .search-title {
-    font-weight: 500;
-  }
+.search-warning {
+  background: #fffceb;
+  color: #e28327;
+  border-color: rgba(226, 131, 39, .3);
+  margin-bottom: .15rem;
+  padding: .05rem;
+  border: .01rem solid rgba(45, 112, 145, .3);
+  border-radius: .04rem;
+  text-shadow: 0 .01rem 0 #fff;
+}
 
-  .search-content {
+.search-item {
+  margin-bottom: .4rem;
+}
 
-  }
+.search-title {
+  font-weight: 500;
+}
 
-  </style>
+.search-content {}
+</style>
     
